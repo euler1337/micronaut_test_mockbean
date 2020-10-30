f719d3911fcd9a7ccfa27bf699e7114b5867db73
Michael Andersson <mican817@gmail.com>
2020-10-30 15:10:40 +0100

    now it works, no more issues

--- complete/src/test/java/example/micronaut/HelloControllerTest.java
+++ complete/src/test/java/example/micronaut/HelloControllerTest.java
@@ -1,12 +1,24 @@
 package example.micronaut;
 
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.annotation.MicronautTest;
+import javax.inject.Inject;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
-public class HelloControllerTest extends HelloControllerTestBase {
+@MicronautTest
+class HelloControllerTest extends HelloControllerTestBase {
+
+    @Inject
+    HelloService helloService;
+
+    @Inject
+    @Client("/")
+    RxHttpClient client;
 
     @Test
     public void testHello() {
--- complete/src/test/java/example/micronaut/HelloControllerTestBase.java
+++ complete/src/test/java/example/micronaut/HelloControllerTestBase.java
@@ -1,25 +1,13 @@
 package example.micronaut;
 
-import io.micronaut.http.client.RxHttpClient;
-import io.micronaut.http.client.annotation.Client;
-import io.micronaut.test.annotation.MicronautTest;
 import io.micronaut.test.annotation.MockBean;
-import javax.inject.Inject;
 import static org.mockito.Mockito.mock;
 
-@MicronautTest
-public abstract class HelloControllerTestBase {
+abstract class HelloControllerTestBase {
 
-    @Inject
-    HelloService helloService;
-
-    @MockBean(HelloServiceImpl.class)
+    @MockBean(HelloService.class)
     HelloService helloService() {
         return mock(HelloService.class);
     }
-
-    @Inject
-    @Client("/")
-    RxHttpClient client;
 
 }
